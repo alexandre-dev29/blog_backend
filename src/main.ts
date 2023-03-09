f407589470ceacd94e85a5bf65ffaa1c8a5c2472
alexandre-dev29 <axel.business29@gmail.com>
2023-03-09 12:17:48 +0200

    adding some libs for security and configure swagger

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,22 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Community Blog api')
+    .setDescription('A little description to go herejkm')
+    .setVersion('1.0')
+    .addTag('blog')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3005);
 }
 
