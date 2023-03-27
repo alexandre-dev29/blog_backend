d1506f8cf90e9e34dafe63aaf074c4b2e66f823c
alexandre-dev29 <axel.business29@gmail.com>
2023-03-27 16:06:28 +0200

    dynamic port for deploying

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.PORT || 3005);
+  await app.listen(process.env.PORT);
 }
 
 bootstrap();
